de8913ba1ebc487efe33982305e4e5b9234131da
felipel7 <felipelseven@gmail.com>
2021-10-30 23:40:11 -0300

    chore: created Domain component

---
+++ src/components/Domain/Domain.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { MdOutlineYoutubeSearchedFor } from "react-icons/md";
+
+import styles from "./Domain.module.scss";
+
+export default function Domain() {
+  const [domains, setDomains] = useState([
+    { id: 1, text: ".com $9", isSelected: false },
+    { id: 2, text: ".sg $12", isSelected: true },
+    { id: 3, text: ".space $7", isSelected: false },
+    { id: 4, text: ".info $14", isSelected: false },
+    { id: 5, text: ".net $11", isSelected: false },
+    { id: 6, text: ".xyz $10", isSelected: false },
+  ]);
+
+  function handleSelected(event, id) {
+    event.preventDefault();
+
+    const index = domains.findIndex(domain => domain.id === id);
+
+    const newDomains = domains.map(domain => {
+      domain.isSelected = false;
+      return {
+        ...domain,
+      };
+    });
+
+    newDomains[index].isSelected = true;
+    setDomains(newDomains);
+  }
+
+  return (
+    <div className={styles.domain}>
+      <header>
+        <h2>Starting at $10 per month</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+          totam!
+        </p>
+      </header>
+      <div>
+        <input type="text" placeholder="Enter domain name here..." />
+        <button>
+          <MdOutlineYoutubeSearchedFor size={25} />
+          Search
+        </button>
+      </div>
+      <ul>
+        {domains.map(domain => (
+          <li key={domain.id}>
+            <a
+              href="/"
+              onClick={event => handleSelected(event, domain.id)}
+              className={domain.isSelected ? `${styles.active}` : ""}
+            >
+              {domain.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import Domain from "../components/Domain/Domain";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <Domain />
     </div>
   );
 }
